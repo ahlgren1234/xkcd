c58907dc82be290b33af0a7c35c1ebaedb3f88cb
Peter Ahlgren <ahlgren1234@gmail.com>
2022-03-17 10:07:33 +0100

    Initial Commit

---
+++ xkcd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xkcd
+//
+//  Created by Peter Ahlgren on 2022-03-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xkcd/xkcdApp.swift
@@ -0,0 +1,17 @@
+//
+//  xkcdApp.swift
+//  xkcd
+//
+//  Created by Peter Ahlgren on 2022-03-17.
+//
+
+import SwiftUI
+
+@main
+struct xkcdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
